6c2d751a6104c5452f413090837fc634978330d9
tatiyasmith <100260928+tatiyasmith@users.noreply.github.com>
2023-04-09 15:08:23 +0700

    Add files via upload

--- app_predict_penguin.py
+++ app_predict_penguin.py
@@ -5,29 +5,60 @@ import pickle
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 import streamlit as st 
+import matplotlib.pyplot as plt
+import plotly.graph_objects as px
 
-model = pickle.load(open('model.penguins.sav','rb'))
-species_encoder = pickle.load(open('encoder.species.sav','rb'))
-island_encoder = pickle.load(open('encoder.island.sav','rb'))
-sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
-evaluations = pickle.load(open('evals.all.sav','rb'))
+st.header('Penguin Species Prection')
+st.header('Create By : Tatiya Pha-Ngam ID : 64130701712 ')
+tab1, tab2, tab3 = st.tabs(["Prediction", "Evaluation", "About"])
 
-st.title('Penguin Species Prection :) ')
+with tab1:
+    model = pickle.load(open('model.penguins.sav','rb'))
+    species_encoder = pickle.load(open('encoder.species.sav','rb'))
+    island_encoder = pickle.load(open('encoder.island.sav','rb'))
+    sex_encoder = pickle.load(open('encoder.sex.sav','rb'))
+    evaluations = pickle.load(open('evals.all.sav','rb'))
 
-x1 = st.radio('Select island', island_encoder.classes_)
-x1 = island_encoder.transform([x1])[0]
-# x1 
-x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
-x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
-x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
-x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
-x6 = st.radio("เลือก sex ",sex_encoder.classes_)
-x6 = sex_encoder.transform([x6])[0]
+    st.header('Penguin Species Prection :) ')
 
-x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
-             columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
+    x1 = st.radio('Select island', island_encoder.classes_)
+    x1 = island_encoder.transform([x1])[0]
+    # x1 
+    x2 = st.slider('Select culmen length (mm)', 25, 70, 40)
+    x3 = st.slider("เลือก culmen depth (mm)", 10,30,15 )
+    x4 = st.slider("เลือก flipper length (mm)", 150,250,200)
+    x5 = st.slider("เลือก body mass (g)", 2500,6500,3000)
+    x6 = st.radio("เลือก sex ",sex_encoder.classes_)
+    x6 = sex_encoder.transform([x6])[0]
 
-pred = model.predict(x_new)
+    x_new = pd.DataFrame(data=np.array([x1, x2, x3, x4, x5, x6]).reshape(1,-1), 
+                 columns=['island', 'culmen_length_mm', 'culmen_depth_mm','flipper_length_mm', 'body_mass_g', 'sex'])
 
-st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+    pred = model.predict(x_new)
+
+    st.write('Predicted Species: ' , species_encoder.inverse_transform(pred)[0])
+with tab2:
+    x = evaluations.columns
+    fig = px.Figure(data=[
+        px.Bar(name = 'Decision Tree',
+               x = x,
+               y = evaluations.loc['Decision Tress']),
+        px.Bar(name = 'Random Forest',
+               x = x,
+               y =  evaluations.loc['Random Forest']),
+        px.Bar(name = 'KNN',
+               x = x,
+               y =  evaluations.loc['KNN']),
+        px.Bar(name = 'AdaBoost',
+               x = x,
+               y =  evaluations.loc['AdaBoost']),
+        px.Bar(name = 'XGBoost',
+               x = x,
+               y =  evaluations.loc['XGBoost'])
+    ])
+    st.plotly_chart(fig, use_container_width=True)
+
+    st.dataframe(evaluations)
+with tab3:
+    st.header('About:) ')
 
